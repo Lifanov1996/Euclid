2eed9df491f4d323689dfd412d8a7123d1ed08bf
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-01-07 12:45:03 +0300

    Added tabs in section work, Added js tabs

--- skript.js
+++ skript.js
@@ -1,3 +1,4 @@
+/*  бургер меню  */
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.header__nav');
 let menuLinks = menu.querySelectorAll('.nav__link');
@@ -17,5 +18,25 @@ menuLinks.forEach(function (el) {
     menu.classList.remove('header__nav--active');
 
     document.body.classList.remove('stop__scroll');
+  });
+});
+
+/*  табы в секции “Как мы работаем”  */
+let tabsBtn = document.querySelectorAll('.work__link');
+let tabsItem = document.querySelectorAll('.work__block');
+
+tabsBtn.forEach(function(element){
+  element.addEventListener('click', function(e){
+    const path = e.currentTarget.dataset.path;
+
+    tabsBtn.forEach(function(btn){
+      btn.classList.remove('work__link--active');
+    });
+    e.currentTarget.classList.add('work__link--active');
+
+    tabsItem.forEach(function(element){
+      element.classList.remove('work__block--active');
+    });
+    document.querySelector(`[data-target="${path}"]`).classList.add('work__block--active');
   });
 });
